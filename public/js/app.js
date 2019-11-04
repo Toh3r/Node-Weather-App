9fd03ad50262078ce37e96bf500ca7da0bd0b444
Toh3r <dan.t@live.ie>
2019-11-04 18:31:51 +0000

    Add comments to js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,26 @@
+// DOM queries
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#messageOne');
 const messageTwo = document.querySelector('#messageTwo');
 
+// When form clicked
 weatherForm.addEventListener('submit', e => {
+  //Dont refresh page
   e.preventDefault();
   const location = search.value;
 
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
 
+  // API url
   const url = `http://localhost:3000/weather?address=${location}`;
 
+  // HTTP req in browser
   fetch(url).then(res => {
+    // Parse json
     res.json().then(data => {
+      // Change paragraphs depending on retrieved info
       if (data.error) {
         messageOne.textContent = `Error: ${data.error}`;
         messageTwo.textContent = '';
